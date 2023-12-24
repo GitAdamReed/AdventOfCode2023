849103534f80f1786d7e2f66dab06e9193d2508b
GitAdamReed <adamreed170@gmail.com>
2023-12-24 19:43:40 +0000

    Completed Day 4 Part 2

--- AdventOfCode2023/AdventOfCode2023/Classes/Day4.cs
+++ AdventOfCode2023/AdventOfCode2023/Classes/Day4.cs
@@ -59,11 +59,67 @@ namespace AdventOfCode2023.Classes
             return sum;
         }
 
+
+        private static int GetNumOfCards()
+        {
+            int totalCards = 0;
+            List<string> lines = File.ReadLines(_inputPath).ToList();
+            Dictionary<int, string[]> cardDict = new();
+            Dictionary<int, int> cardCopies = new();
+
+            // Initialise card dict
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string[] cardArr = lines[i].Split(" ");
+                cardDict.Add(i, cardArr);
+            }
+
+            // Initialise card copy dict
+            for (int i = 0; i < cardDict.Count; i++)
+            {
+                cardCopies.Add(i, 1);
+            }
+
+            foreach (var kvp in cardDict)
+            {
+                List<int> winningNums = new();
+                List<int> myNums = new();
+                List<int> matchingNums = new();
+                bool isMyNums = false;
+                foreach (string element in kvp.Value)
+                {
+                    if (Int32.TryParse(element, out int result))
+                    {
+                        if (isMyNums) myNums.Add(result);
+                        else winningNums.Add(result);
+                    }
+                    if (element == "|") isMyNums = true;
+                }
+
+                foreach (int num in myNums)
+                {
+                    if (winningNums.Contains(num))
+                    {
+                        matchingNums.Add(num);
+                    }
+                }
+
+                for (int i = 1; i <= matchingNums.Count; i++)
+                {
+                    if (i >= cardDict.Count) break;
+                    cardCopies[kvp.Key + i] += cardCopies[kvp.Key];
+                }
+                totalCards += cardCopies[kvp.Key];
+            }
+
+            return totalCards;
+        }
+
         
         protected override void CreateOutput()
         {
             File.WriteAllText(_outputPath, $"Part 1: {GetTotalPoints()}");
-            //File.AppendAllText(_outputPath, $"\nPart 2: {GetGearRatioSum()}");
+            File.AppendAllText(_outputPath, $"\nPart 2: {GetNumOfCards()}");
         }
     }
 }
